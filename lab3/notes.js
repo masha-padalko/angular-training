f298f39ad8054f8eb287fcbf12ffa94d9d66f0ec
GREY2 <GREY2@ptc.local>
2016-12-05 13:42:22 +0200

    event mouseEnter

--- lab3/notes.js
+++ lab3/notes.js
@@ -2,6 +2,10 @@ angular.module('myapp', []);
 
 angular.module('myapp').controller('NotesController',
     function($scope) {
-    $scope.checked=true;
+        $scope.checked=false;
+        $scope.data='';
         $scope.notes = [{name: "first", text: "bla bla"}, {name: "second",text: "bla bla 2"} ];
+        $scope.hover = function($event){
+            $scope.data=$event.clientX;
+        }
     });
