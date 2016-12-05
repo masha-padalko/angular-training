c3590541fcbbacad134557ffc056a8fca81d2941
GREY2 <GREY2@ptc.local>
2016-12-05 10:44:34 +0200

    first commit

---
+++ lab1/hello.js
@@ -0,0 +1,10 @@
+
+angular.module("myapp", [])
+    .controller("HelloController", function($scope) {
+        $scope.greeting = "";
+        $scope.update = function() {
+            if ($scope.name) {
+                $scope.greeting = "Hello, "+$scope.name+"!";
+            }
+        }
+    });
